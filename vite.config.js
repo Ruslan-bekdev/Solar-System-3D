3aa18ece40482a9aef323db42319cb26020da497
Ruslan-bekdev <toopikoliko2@gmail.com>
2025-08-04 22:33:39 +0600

    деплой

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import {defineConfig} from 'vite';
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	root: './',
+	base: 'https://ruslan-bekdev.github.io/codenote/',
+	build: {
+		outDir: 'dist',
+		sourcemap: true,
+	},
+	server: {
+		port: 3000,
+	},
+	plugins: [react()],
+});
